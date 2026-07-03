e45012cbd88572d9d201f0d876a197ec340301e5
maryamabdallah <maryamabdallah698@gmail.com>
2026-07-03 18:09:07 +0300

    refactor: delete the mock learn folder

--- lib/features/student/learn/data/datasources/learn_remote_datasource.dart
+++
@@ -1,5 +0,0 @@
-import 'package:elara/features/student/learn/data/models/student_group_model.dart';
-
-abstract class LearnRemoteDataSource {
-  Future<List<StudentGroupModel>> getGroups();
-}
--- lib/features/student/learn/domain/repositories/learn_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:elara/core/network/api_result.dart';
-import 'package:elara/features/student/learn/domain/entities/student_group.dart';
-
-abstract class LearnRepository {
-  Future<ApiResult<List<StudentGroup>>> getGroups();
-}
